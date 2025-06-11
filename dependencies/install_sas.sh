02c2b2587e95c67182baf1087c109e1f968c9af5
Juancho <juanjqogm@gmail.com>
2025-06-11 16:02:58 +0100

    [install_sas.h] added a line to create the folder ros2_ws/src

--- dependencies/install_sas.sh
+++ dependencies/install_sas.sh
@@ -3,6 +3,7 @@
 # Install SAS
 
 # Suggested instructions to clone SAS
+mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_msgs.git --recursive 
 cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_core.git --recursive 
 cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_common.git --recursive 
