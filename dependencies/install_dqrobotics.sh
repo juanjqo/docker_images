664bb7003bf17d898957fa913ec64677b0dc23aa
Juancho <juanjqogm@gmail.com>
2025-06-11 14:29:48 +0100

    Fixed the execution of the script in the dockerfile

--- dependencies/install_dqrobotics.sh
+++ dependencies/install_dqrobotics.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Install DQ Robotics dependencies
+
+# Install dependencies (including additional packages for development)
+apt-get update && apt-get install -y libeigen3-dev libzmq3-dev libboost-all-dev python3-pybind11 python3-pip 
+
+
+# Install qpOASES
+mkdir -p ~/utils
+cd ~/utils && git clone https://github.com/coin-or/qpOASES.git
+cd ~/utils/qpOASES && sed -i -e 's/option(BUILD_SHARED_LIBS "If ON, build shared library instead of static" OFF)/option(BUILD_SHARED_LIBS "If ON, build shared library instead of static" ON)/g' CMakeLists.txt
+cd ~/utils/qpOASES/ && mkdir -p build
+cd ~/utils/qpOASES/build  && cmake .. && make && make install
+
 # Install DQ Robotics (development branch)
 add-apt-repository ppa:dqrobotics-dev/development 
-apt-get update && apt-get install -y libdqrobotics libdqrobotics-interface-json11 libdqrobotics-interface-coppeliasim libdqrobotics-interface-coppeliasim-zmq
+apt-get update && apt-get install -y libdqrobotics libdqrobotics-interface-json11 \
+libdqrobotics-interface-coppeliasim libdqrobotics-interface-coppeliasim-zmq libdqrobotics-interface-qpoases
 python3 -m pip install dqrobotics --pre --break-system-packages
 
 
