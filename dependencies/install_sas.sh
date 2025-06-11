2871c51f8b992c8398b7783d1177b00b2db90bec
Juancho <juanjqogm@gmail.com>
2025-06-11 15:29:03 +0100

    Removed the sas folder to clone the submodules

--- dependencies/install_sas.sh
+++ dependencies/install_sas.sh
@@ -3,9 +3,9 @@
 # Install SAS
 
 # Suggested instructions to clone SAS
-cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_msgs.git --recursive sas
-cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_core.git --recursive sas
-cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_common.git --recursive sas
-cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_datalogger.git --recursive sas
-cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_conversions.git --recursive sas
-cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_robot_driver.git --recursive sas
+cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_msgs.git --recursive 
+cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_core.git --recursive 
+cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_common.git --recursive 
+cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_datalogger.git --recursive 
+cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_conversions.git --recursive 
+cd ~/ros2_ws/src && git clone -b jazzy https://github.com/SmartArmStack/sas_robot_driver.git --recursive 
