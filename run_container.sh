9173198431dc1fc8d3b6568eb1a5cb68f826b9ff
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2025-06-12 11:44:57 +0100

    Added a runner script

---
+++ run_container.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Name:" $1
+# Argument validation check
+if [ $# -eq 0 ]; then
+    echo "Error: No arguments provided."
+    echo "Example: sh run_container.sh ubuntu_24_dqrobotics"
+    exit 1
+fi
+xhost +local:root
+docker run -it --name=$1 \
+--rm --privileged --network=host --env=DISPLAY \
+ubuntu_24_dqrobotics /bin/bash
